295b67803eb71e8db0d0c3502d5517999235e589
arthurboucard <arthur.boucard@hotmail.fr>
2022-09-20 09:30:40 +0200

    added topbar with max min and close window fonctionning

--- main.js
+++ main.js
@@ -3,19 +3,23 @@ const { ipcMain } = require('electron')
 
 const createWindow = () => {
 	const win = new BrowserWindow({
+		titleBarStyle: 'hidden',
+		titleBarOverlay: {
+			color: '#34475a',
+			symbolColor: '#FFFFFF',
+		},
 		width: 800,
 		height: 600,
 		webPreferences: {
 			preload: __dirname + '/preload.js',
 		},
 	});
-	ipcMain.handle('ping', () => 'pong')
 	win.loadFile('index.html');
 
-	const view = new BrowserView()
-	win.setBrowserView(view)
-	view.setBounds({ x: 0, y: 300, width: 800, height: 300 })
-	view.webContents.loadURL('https://www.youtube.com')
+	// const view = new BrowserView()
+	// win.setBrowserView(view)
+	// view.setBounds({ x: 0, y: 300, width: 800, height: 300 })
+	// view.webContents.loadURL('https://www.youtube.com')
 };
 
 app.whenReady().then(() => {
