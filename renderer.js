9d15de4c6915da567b0143aa870717cc3e905c0e
arthurboucard <arthur.boucard@hotmail.fr>
2022-09-22 10:10:33 +0200

    commented not working code used to make the ipc scalable based on config.js

--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
+// import config from './config'
+
 document.getElementById('menu-btn').addEventListener('click', () => {
     window.ipcRender.send('message:main');
 });
@@ -9,3 +11,9 @@ document.getElementById('youtube-btn').addEventListener('click', () => {
 document.getElementById('netflix-btn').addEventListener('click', () => {
     window.ipcRender.send('message:netflix');
 });
+
+// for (key in config.app) {
+//     document.getElementById(key + '-btn').addEventListener('click', () => {
+//         window.ipcRender.send('message:' + key);
+//     });
+// }
