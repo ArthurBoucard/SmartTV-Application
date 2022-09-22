5b18b149114ca452f10cdff27e4347140490d7c2
arthurboucard <arthur.boucard@hotmail.fr>
2022-09-22 11:11:02 +0200

    changed front end structure + button display flex

--- config.js
+++ config.js
@@ -1,6 +1,33 @@
 var config = {
-    "app": {
-        "youtube": true,
-        "netflix": true
+    app: {
+        youtube: true,
+        netflix: true,
+        netflizsdx: true,
+        netflizdx: true,
+        netflsdx: true,
+        netfizsdx: true,
+        netlizsdx: true,
+        neflizsdx: true,
+        ntflizsdx: true,
+        netflizs: true,
+        netflidx: true,
+        netfzsdx: true,
+        nelizsdx: true,
+        nelizsdx: true,
+        netzdflizsdx: true,
+        netflizsdzx: true,
+        netflzdizzsdx: true,
+        netflizdzsdx: true,
+        netflizsdx: true,
+        netflizsddzx: true,
+    },
+    widget: {
+        clock: {
+            status: true,
+            position: "top_left",
+            format: "",
+            timezone: "",
+            design: "",
+        }
     }
 }
