ac7cb7a79381311b05366eedb0731360c462e750
arthurboucard <arthur.boucard@hotmail.fr>
2022-12-15 18:22:38 +0800

    modified config format

--- config.js
+++ config.js
@@ -1,17 +1,37 @@
 var config = {
     app: {
-        youtube: true,
-        netflix: true,
-        disney: true,
-        amazon_prime: true
+        youtube: {
+            status: true,
+            url: "https://www.youtube.com"
+        },
+        netflix:  {
+            status: true,
+            url: "https://www.netflix.com"
+        },
+        disney:  {
+            status: true,
+            url: "https://www.disneyplus.com"
+        },
+        amazon_prime:  {
+            status: false,
+            url: "https://www.primevideo.com"
+        }
     },
     widget: {
         clock: {
             status: true,
             position: "top_left",
-            format: "",
-            timezone: "",
-            design: "",
+            format: "HH:MM",
+            timezone: "UTC+1",
+            design: "basic",
+        },
+        weather: {
+            status: true,
+            position: "bottom_right",
+            city: "Lyon",
+            country: "France",
+            temperature_format : "C",
+            design: "basic",
         }
     }
 }
